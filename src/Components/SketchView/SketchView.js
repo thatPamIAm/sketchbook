d7a706b55abb49bbca6afa9b97d4f46d9d5e41b2
sertmer <scottertmer@gmail.com>
2020-02-29 11:03:34 -0700

    Set sketchView to display multiple sketches

--- src/Components/SketchView/SketchView.js
+++ src/Components/SketchView/SketchView.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import P5Wrapper from 'react-p5-wrapper'
 import './SketchView.scss'
-import sketch from '../../Sketches/sketch_02_29_2020';
+import * as sketches from '../../Sketches/sketch_02_29_2020';
 import rightArrow from '../../Assets/Images/002-next.svg'
 import leftArrow from '../../Assets/Images/001-back.svg'
 
 const SketchView = () => {
-
+  const sketchNames = Object.keys(sketches)
+  console.log(sketchNames)
+  const [count, setCount] = useState(0)
   return (
     <div className='sketch-view'>
       <div className='sketch-and-nav'>
-        <img className='arrow' src={leftArrow} />
-        <P5Wrapper sketch={sketch} />
-        <img className='arrow' src={rightArrow} />
+        <img
+          className='arrow'
+          src={leftArrow}
+          onClick={() => setCount(count - 1)}
+        />
+        <P5Wrapper sketch={sketches[sketchNames[count]]} />
+        <img
+          className='arrow'
+          src={rightArrow}
+          onClick={() => setCount(count + 1)}
+        />
       </div>
       <div className='signature'>
         <p>s. ertmer</p>
--- src/Sketches/sketch_02_29_2020.js
+++ src/Sketches/sketch_02_29_2020.js
@@ -1,5 +1,6 @@
-export default function sketch(p) {
+export function sketch(p) {
   let canvas
+
   p.setup = () => {
     canvas = p.createCanvas(500, 500)
     p.noCursor()
@@ -13,5 +14,24 @@ export default function sketch(p) {
     p.background(p.pmouseY / 2, 100, 100)
     p.fill(360 - p.pmouseY / 2, 100, 100)
     p.rect(250, 250, p.pmouseX + 1, p.pmouseX + 1)
+  }
+}
+
+export function sketch1(p) {
+  let canvas
+
+  p.setup = () => {
+    canvas = p.createCanvas(500, 500)
+    p.noCursor()
+
+    p.colorMode(p.HSB)
+    p.rectMode(p.CENTER)
+    p.noStroke()
+  }
+
+  p.draw = () => {
+    p.background(p.pmouseY / 2, 100, 100)
+    p.fill(360 - p.pmouseY / 2, 100, 100)
+    p.ellipse(250, 250, p.pmouseX + 1, p.pmouseX + 1)
   }
 }
