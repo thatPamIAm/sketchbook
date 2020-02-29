a970cd26c90d1c2fe18aac9a211a2007bf224e3e
sertmer <scottertmer@gmail.com>
2020-02-29 10:28:06 -0700

    Add and style arrows

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.scss';
 import SketchView from '../SketchView/SketchView'
-
 const App = () => {
   return (
     <div className="App">
--- src/Components/SketchView/SketchView.js
+++ src/Components/SketchView/SketchView.js
@@ -2,12 +2,18 @@ import React from 'react';
 import P5Wrapper from 'react-p5-wrapper'
 import './SketchView.scss'
 import sketch from '../../Sketches/sketch_02_29_2020';
+import rightArrow from '../../Assets/Images/002-next.svg'
+import leftArrow from '../../Assets/Images/001-back.svg'
 
 const SketchView = () => {
 
   return (
     <div className='sketch-view'>
-      <P5Wrapper sketch={sketch} />
+      <div className='sketch-and-nav'>
+        <img className='arrow' src={leftArrow} />
+        <P5Wrapper sketch={sketch} />
+        <img className='arrow' src={rightArrow} />
+      </div>
       <div className='signature'>
         <p>s. ertmer</p>
       </div>
